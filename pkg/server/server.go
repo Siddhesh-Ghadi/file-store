3d052aa3ec79d01c72b9bb5fa3da82ec0c77265f
Siddhesh Ghadi <sghadi1203@gmail.com>
2022-12-11 21:23:16 +0530

    Use common function to return server error

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -22,19 +22,16 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("[INFO] Invoked / by ", r.RemoteAddr)
 
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("Welcome to File Store"))
 }
 
 func lsHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("[INFO] Invoked /ls by ", r.RemoteAddr)
-	w.Header().Set("Content-Type", "application/json")
 	
 	fileNames, err := fileutil.GetAllFileNames(serverDir)
 	if err != nil {
-		log.Print("[ERR] ",err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Something went wrong."))
+		serverError(w, err)
 		return
 	}
 
@@ -44,12 +41,11 @@ func lsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
-		log.Printf("[ERR] Error happened in JSON marshal. Err: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Something went wrong."))
+		serverError(w, err)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonResp)
 }
 
