b1f35abc54b68157968f1b026a691ad40b1b9444
Siddhesh Ghadi <sghadi1203@gmail.com>
2022-12-12 11:02:24 +0530

    Add endpoint to upload files

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"fmt"
 	"strconv"
+	"io"
+	"os"
 	"net/http"
 	"encoding/json"
 	"github.com/Siddhesh-Ghadi/file-store/pkg/fileutil"
@@ -16,7 +18,14 @@ func serverError(w http.ResponseWriter, err error) {
 	log.Print("[ERR] ",err)
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Something went wrong."))
+	w.Write([]byte("An error occurred. Please try again."))
+}
+
+func clientError(w http.ResponseWriter, err error) {
+	log.Print("[ERR] ",err)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Bad Request. Please check your input."))
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -135,6 +144,67 @@ func freqWordsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonResp)
 }
 
+// 409 - if file exist
+// 201 - upload successful
+// 400 - missing/incorrect params in request.
+// 500 - server error 
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("[INFO] Invoked /add by ", r.RemoteAddr)
+	if r.Method != "Post" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+    // left shift 32 << 20 which results in 32*2^20 = 33554432
+	// x << y, results in x*2^y
+	err := r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		clientError(w, err)
+		return 
+	}
+	name := r.Form.Get("name")
+	// Retrieve the file from form data
+	f, _, err := r.FormFile("file")
+	if err != nil {
+		clientError(w, err)
+		return  
+	}
+	defer f.Close()
+
+	// check if file exist based on file name
+	fileNames, err := fileutil.GetAllFileNames(serverDir)
+	if err != nil {
+		serverError(w, err)
+		return
+	}
+	for _, fn := range fileNames{
+		if fn == name{
+			w.WriteHeader(http.StatusConflict)
+			w.Header().Set("Content-Type", "text/plain")
+			w.Write([]byte(name+" already exists."))
+			return
+		}
+	}
+
+	fullPath := serverDir + "/" + name
+	file, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		serverError(w, err)
+		return  
+	}
+	defer file.Close()
+	// Copy the file to the destination path
+	_, err = io.Copy(file, f)
+	if err != nil {
+		serverError(w, err)
+		return 
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(name+" uploaded successfully"))
+}
+
 func Start(port string, dir string){
 	serverDir = dir
 
@@ -146,6 +216,7 @@ func Start(port string, dir string){
 	http.HandleFunc("/ls", lsHandler)	// list all files
 	http.HandleFunc("/wc", wcHandler)	// return number of words from all files
 	http.HandleFunc("/freq-words", freqWordsHandler)	// return number of occurrences of words from all files
+	http.HandleFunc("/add", addHandler)	// upload a file
 
 	// start server
 	log.Print("[INFO] Server listening on ", port)
